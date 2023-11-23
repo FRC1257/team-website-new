5958a8e73cd202aaadbcf56f72972998d87625ce
derp000 <84947701+derp000@users.noreply.github.com>
2023-11-23 11:16:45 -0500

    Fix Footer using process.env

--- SnailSite/src/components/Footer.tsx
+++ SnailSite/src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div>
         <h1>Footer</h1>
-        <img src={process.env.PUBLIC_URL + "/img/logos/snail_black.png"} alt="2023_bot.JPG" height={"200"} />
+        <img src={"/img/logos/snail_black.png"} alt="2023_bot.JPG" height={"200"} />
         </div>
     );
 };
