3a68e23ed8cd17bef385e1b1f04b43535397b6ff
derp000 <84947701+derp000@users.noreply.github.com>
2023-12-30 00:13:24 -0500

    Add placeholder text on carousel

--- SnailSite/src/components/EmblaCarousel.tsx
+++ SnailSite/src/components/EmblaCarousel.tsx
@@ -14,10 +14,17 @@ type PropType = {
 // https://www.embla-carousel.com/plugins/autoplay/
 const EmblaCarousel: React.FC<PropType> = (props) => {
   const { slides, options } = props;
-  const [emblaRef] = useEmblaCarousel(options, [Autoplay({delay: 2000})]);
+  const [emblaRef] = useEmblaCarousel(options, [Autoplay({ delay: 2000 })]);
 
   return (
-    <div className="embla">
+    <div className="relative embla">
+      <div className="absolute left-10 top-52 z-50 text-white flex flex-col items-start w-1/3 gap-4">
+        <h1 className="text-6xl font-bold">UCVTS FRC Robotics Team 1257</h1>
+        <h2 className="text-4xl font-semibold">
+          We are the coolest FRC Robotics Team in New Jersey.
+        </h2>
+        <button className="bg-green-800 hover:bg-green-700 rounded-xl py-4 px-6 font-bold text-lg mt-4">Learn more</button>
+      </div>
       <div className="embla__viewport" ref={emblaRef}>
         <div className="embla__container">
           {slides.map((index) => (
