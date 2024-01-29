70a90b9c75927ebfc2c77fbd93bd3bd24b48319b
derp000 <84947701+derp000@users.noreply.github.com>
2024-01-29 16:46:12 -0500

    Make homepage sections responsive

--- SnailSite/src/components/Section.tsx
+++ SnailSite/src/components/Section.tsx
@@ -10,12 +10,13 @@ export interface SectionProps {
 
 const Section = ({ textLeft, heading, body, imgSrc, button }: SectionProps) => {
   return (
-    <div className="grid grid-cols-2 gap-20 bg-green-300 px-24 py-12">
-      <div className={`bg-red-300 ${textLeft ? "order-1" : ""}`}>
-        <img className="h-[480px] w-full" src={imgSrc} alt="myimg" />
+    <div className="grid lg:grid-cols-2 lg:gap-20 gap-10 bg-green-300 lg:px-24 lg:py-12 px-6 py-6">
+      <div className={`bg-red-300 ${textLeft ? "lg:order-1" : ""}`}>
+        <div className="text-6xl text-center lg:text-left block lg:hidden">{heading}</div>
+        <img className="lg:block hidden h-[480px] w-full" src={imgSrc} alt="myimg" />
       </div>
       <div className="bg-blue-300">
-        <div className="text-6xl mb-10">{heading}</div>
+        <div className="text-6xl mb-10 hidden lg:block">{heading}</div>
         <div className="text-2xl flex flex-wrap leading-relaxed mb-10">
           {body}
         </div>
