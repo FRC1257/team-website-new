924d618c65d7828f5c8852c336912d6011d37b6f
Akash Dubey <69588353+AkeBoss-tech@users.noreply.github.com>
2024-02-24 22:09:10 -0500

    add components

--- SnailSite/src/pages/About.tsx
+++ SnailSite/src/pages/About.tsx
@@ -1,8 +1,135 @@
-// TODO: change to drawer in mobile view
-// TODO: hover styling
+"use client";
+import React from "react";
+import { twMerge } from "tailwind-merge";
+import Image from "next/image";
+import { SparklesCore } from "../components/ui/sparkles";
+import { TracingBeam } from "../components/ui/tracing-beam";
+import { TextGenerateEffect } from "../components/ui/text-generate-effect";
+import { HeroParallax } from "../components/ui/hero-parallax";
 
-const About = () => {
-  return <div>About</div>;
-};
+export const robots = [
+  {
+    title: "2004 Robot",
+    link: "https://www.thebluealliance.com/team/1257/2004",
+    thumbnail:
+      "images/robots/2004_robot.jpg",
+  },
+  {
+    title: "2011 Robot",
+    link: "https://www.thebluealliance.com/team/1257/2011",
+    thumbnail:
+      "images/robots/2011_robot.jpg",
+  },
+  {
+    title: "2012 Robot",
+    link: "https://www.thebluealliance.com/team/1257/2012",
+    thumbnail:
+      "images/robots/2012_bot.jpg",
+  },
+  {
+    title: "2013 Robot",
+    link: "https://www.thebluealliance.com/team/1257/2013",
+    thumbnail:
+      "images/robots/2013_hanging.jpg",
+  },
+  {
+    title: "2014 Robot",
+    link: "https://www.thebluealliance.com/team/1257/2014",
+    thumbnail:
+      "images/robots/2014_1.JPG",
+  },
+  {
+    title: "2015 Robot",
+    link: "https://www.thebluealliance.com/team/1257/2015",
+    thumbnail:
+      "images/robots/2015_stacking.jpg",
+  },
+  {
+    title: "2016 Robot",
+    link: "https://www.thebluealliance.com/team/1257/2016",
+    thumbnail:
+      "images/robots/2016_bot.JPG",
+  },
+  {
+    title: "2017 Robot",
+    link: "https://www.thebluealliance.com/team/1257/2017",
+    thumbnail:
+      "images/robots/2017_robot.jpg",
+  },
+  {
+    title: "2018 Robot",
+    link: "https://www.thebluealliance.com/team/1257/2018",
+    thumbnail:
+      "images/robots/2018_bad_horse.jpg",
+  },
+  {
+    title: "2019 Robot",
+    link: "https://www.thebluealliance.com/team/1257/2019",
+    thumbnail:
+      "images/robots/2019_shooting_shark.jpg",
+  },
+  {
+    title: "2020 Robot",
+    link: "https://www.thebluealliance.com/team/1257/2020",
+    thumbnail:
+      "images/robots/2020_robot.jpg",
+  },
+  {
+    title: "2022 Robot",
+    link: "https://www.thebluealliance.com/team/1257/2022",
+    thumbnail:
+      "images/robots/2022.JPG",
+  },
+  {
+    title: "2023 Robot",
+    link: "https://www.thebluealliance.com/team/1257/2023",
+    thumbnail:
+      "images/robots/2023_bot.jpg",
+  },
+];
+
+
+function About() {
+  return (
+    <><div className="h-[40rem] relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+      <div className="w-full absolute inset-0 h-screen">
+        <SparklesCore
+          id="tsparticlesfullpage"
+          background="transparent"
+          minSize={0.6}
+          maxSize={1.4}
+          particleDensity={100}
+          className="w-full h-full"
+          particleColor="#FFFFFF" />
+      </div>
+      <h1 className="md:text-7xl text-3xl lg:text-6xl font-bold text-center text-white relative z-20">
+        Parallel Universe
+      </h1>
+    </div>
+    <TracingBeam className="">
+      <div key={`content-0`} className="mb-10">
+
+        <p className={twMerge("text-xl mb-4")}>
+          Founded in 2006
+        </p>
+
+        <div className="text-sm  prose prose-sm dark:prose-invert">
+          <Image
+            src="/images/FIRST_logo.png"
+            alt="blog thumbnail"
+            height="1000"
+            width="1000"
+            className="rounded-lg mb-10 object-cover"
+          />
+          
+          We love FIRST.
+        </div>
+      </div>
+      <TextGenerateEffect words="My brother what the heck is going on. I love robots and robotics. My name is Turbo the snail nice to meet you" />
+      <HeroParallax products={robots}/>
+    </TracingBeam>
+    </>
+  );
+}
 
 export default About;
--- SnailSite/tailwind.config.js
+++ SnailSite/tailwind.config.js
@@ -1,6 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "/index.html",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   mode: "jit",
   theme: {
     extend: {
@@ -94,7 +105,31 @@ export default {
           DEFAULT: "#f3ffe5",
         },
       },
+      keyframes: {
+        shimmer: {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    addVariablesForColors,
+  ],
 };
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme('colors'));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ':root': newVars,
+  });
+}
