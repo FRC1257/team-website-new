7da560ccdebdfe9fef3edfdd10d52bc2992ca40e
derp000 <84947701+derp000@users.noreply.github.com>
2024-04-04 16:28:31 -0400

    Finish local outreach mostly

--- SnailSite/src/pages/outreachPages/LocalOutreachRouter.tsx
+++ SnailSite/src/pages/outreachPages/LocalOutreachRouter.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import LocalOutreach from "./LocalOutreach";
 import Hydrocar from "./Hydrocar";
 import SummerCamp from "./SummerCamp";
+import ScoutTroops from "./ScoutTroops";
+import LocalSponsors from "./LocalSponsors";
 
 const LocalOutreachRouter = () => {
   return (
@@ -10,6 +12,8 @@ const LocalOutreachRouter = () => {
         <Route path="" element={<LocalOutreach />} />
         <Route path="hydrocar" element={<Hydrocar />} />
         <Route path="summercamp" element={<SummerCamp />} />
+        <Route path="scouttroops" element={<ScoutTroops />} />
+        <Route path="sponsors" element={<LocalSponsors />} />
       </Routes>
     </>
   );
