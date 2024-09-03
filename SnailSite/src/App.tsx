075ef0e9ffc2777e5517ca9f6158d51f2b2cddad
derp000 <84947701+derp000@users.noreply.github.com>
2024-09-03 10:04:34 -0400

    Update routing with createBrowserRouter

--- SnailSite/src/App.tsx
+++ SnailSite/src/App.tsx
@@ -1,4 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  // BrowserRouter,
+  // Routes,
+  // Route,
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
 
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
@@ -10,7 +17,7 @@ import "./App.css";
 
 import LinkTree from "./pages/LinkTree";
 
-import ParagraphSection from "./components/ParagraphSection";
+// import ParagraphSection from "./components/ParagraphSection";
 import Info from "./pages/Info";
 import Awards from "./pages/Awards";
 // import Outreach from "./pages/Outreach";
@@ -19,29 +26,87 @@ import Sponsors from "./pages/Sponsors";
 import Gallery from "./pages/Gallery";
 import OutreachRouter from "./pages/OutreachRouter";
 
+const Layout = () => (
+  <>
+    <Navbar />
+    <Outlet />
+    <Footer />
+  </>
+);
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "info",
+        element: <Info />,
+      },
+      {
+        path: "awards",
+        element: <Awards />,
+      },
+      {
+        path: "support",
+        element: <Support />,
+      },
+      {
+        path: "sponsors",
+        element: <Sponsors />,
+      },
+      {
+        path: "leadership",
+        element: <Leadership />,
+      },
+      {
+        path: "donate",
+        element: <Donate />,
+      },
+      {
+        path: "linktree",
+        element: <LinkTree />,
+      },
+      {
+        path: "gallery",
+        element: <Gallery />,
+      },
+      {
+        path: "outreach/*",
+        element: <OutreachRouter />,
+      },
+    ],
+  },
+]);
+
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/info" element={<Info />} />
-        <Route path="/awards" element={<Awards />} />
-        <Route path="/outreach/*" element={<OutreachRouter />} />
-        <Route path="/support" element={<Support />} />
-        <Route path="/sponsors" element={<Sponsors />} />
-        <Route path="/notfound" element={<NotFound />} />
-        <Route path="/leadership" element={<Leadership />} />
-        <Route path="/donate" element={<Donate />} />
-        <Route path="/linktree" element={<LinkTree />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="/gallery" element={<Gallery />} />
+    <RouterProvider router={router} />
+    // <BrowserRouter>
+    //   <Navbar />
+    //   <Routes>
+    //     {/* <Route path="/" element={<Home />} /> */}
+    //     {/* <Route path="/info" element={<Info />} /> */}
+    //     {/* <Route path="/awards" element={<Awards />} /> */}
+    //     {/* <Route path="/outreach/*" element={<OutreachRouter />} /> */}
+    //     {/* <Route path="/support" element={<Support />} /> */}
+    //     {/* <Route path="/sponsors" element={<Sponsors />} /> */}
+    //     {/* <Route path="/notfound" element={<NotFound />} /> */}
+    //     {/* <Route path="/leadership" element={<Leadership />} /> */}
+    //     {/* <Route path="/donate" element={<Donate />} /> */}
+    //     {/* <Route path="/linktree" element={<LinkTree />} /> */}
+    //     {/* <Route path="*" element={<NotFound />} /> */}
+    //     {/* <Route path="/gallery" element={<Gallery />} /> */}
 
-        <Route path="/test" element={<ParagraphSection />} />
-        {/* <Route path="/t2" element={<BlobText />} /> */}
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    //     {/* <Route path="/test" element={<ParagraphSection />} /> */}
+    //     {/* <Route path="/t2" element={<BlobText />} /> */}
+    //   </Routes>
+    //   <Footer />
+    // </BrowserRouter>
   );
 }
 
