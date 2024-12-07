60e6c55b077d82739c0bf86a2396bbe94d2072b2
derp000 <84947701+derp000@users.noreply.github.com>
2024-12-07 15:20:43 -0500

    Add descriptions and team image

--- SnailSite/src/components/LeaderProfile.tsx
+++ SnailSite/src/components/LeaderProfile.tsx
@@ -1,21 +1,34 @@
 interface LeaderProfileProps {
   name: string;
   position?: string;
+  schoolgrade?: string;
   image: string;
+  fontsize?: string;
 }
 
 export type LeaderProfilePropsList = LeaderProfileProps[];
 
-const LeaderProfile = ({ name, position, image }: LeaderProfileProps) => {
+const LeaderProfile = ({
+  name,
+  position,
+  schoolgrade,
+  image,
+  fontsize,
+}: LeaderProfileProps) => {
+  let size = "lg:text-2xl text-lg";
+  console.log(fontsize);
+  if (fontsize) size = fontsize;
+
   return (
-    <div className="text-white text-center lg:mb-0 mb-5">
+    <div className="text-white text-center">
       <img
-        className="object-contain 2xl:h-64 2xl:w-64 w-32 h-32 rounded-full justify-self-center mb-2"
+        className="object-contain rounded-full justify-self-center mb-4"
         src={image}
         alt={name}
       />
-      <h2 className="font-bold lg:text-2xl text-lg">{name}</h2>
-      <p className="lg:text-lg">{position}</p>
+      <h2 className={`font-bold ${size} mb-2`}>{name}</h2>
+      <p className="lg:text-xl">{position}</p>
+      <p>{schoolgrade}</p>
     </div>
   );
 };
--- SnailSite/src/pages/Leadership.tsx
+++ SnailSite/src/pages/Leadership.tsx
@@ -1,24 +1,7 @@
-import { ReactElement } from "react";
 import LeaderProfile from "../components/LeaderProfile";
 import leadership from "./leadershipProfiles";
-
-// should refactor
-// e.g., with `flex-row` parent and `w-1/5` children somehow
-const rows: ReactElement[] = [];
-for (let i = 0; i < 4; i++) {
-  rows.push(
-    <div className="flex lg:flex-row flex-col items-center lg:items-start justify-around lg:mb-8 mb-0">
-      {leadership.slice(0 + i * 5, 5 + i * 5).map((leader) => (
-        <LeaderProfile
-          name={leader.name}
-          position={leader.position}
-          image={leader.image}
-          key={leader.name}
-        />
-      ))}
-    </div>
-  );
-}
+import teamphoto from "../../public/pics/photo2425.jpg";
+import "./leadership.css";
 
 const Leadership: React.FC = () => {
   return (
@@ -27,20 +10,37 @@ const Leadership: React.FC = () => {
         <h1 className="lg:text-4xl text-2xl text-white font-bold mx-auto text-center mb-8">
           Student Leadership
         </h1>
-        <div className="flex flex-col">{rows.map((row) => row)}</div>
+        <div className="grid-layout mb-12">
+          {leadership.map((leader) => (
+            <LeaderProfile
+              name={leader.name}
+              position={leader.position}
+              schoolgrade={leader.schoolgrade}
+              image={leader.image}
+              fontsize={leader.fontsize}
+              key={leader.name}
+            />
+          ))}
+        </div>
         <h1 className="lg:text-4xl text-2xl text-white font-bold mx-auto text-center mb-8">
           Advisors
         </h1>
-        <div className="flex flex-row justify-center lg:gap-10 gap-6 lg:mb-8">
+        <div className="flex flex-row justify-center lg:gap-14 gap-6 lg:mb-8 max-w-[48rem] mx-auto">
           <LeaderProfile
             name={"Michele Cook"}
             image={"https://placehold.co/600x600"}
+            schoolgrade={"Teacher of Social Studies, MHS"}
           />
           <LeaderProfile
             name={"Michael Tollin"}
             image={"https://placehold.co/600x600"}
+            schoolgrade={"Teacher of Technology, MHS"}
           />
         </div>
+        <h1 className="lg:text-4xl text-2xl text-white font-bold mx-auto text-center mt-8 mb-8">
+          2024-2025 Team Photo
+        </h1>
+        <img src={teamphoto} alt={"team"} className="rounded-lg" />
       </div>
     </div>
   );
