d53734fa06f940f3b32a809b4a669ab5051ca35a
derp000 <84947701+derp000@users.noreply.github.com>
2025-02-06 19:33:16 -0500

    Add friend tier

--- SnailSite/src/pages/Sponsors.tsx
+++ SnailSite/src/pages/Sponsors.tsx
@@ -1,16 +1,33 @@
-import tshirt from "../../public/pics/tshirt.png";
+import friends from "./sponsorFriendTier";
+import "./Sponsors.css"
 
 const Sponsors = () => {
   return (
-    <div className="bg-background p-10">
-      <h1 className="lg:text-5xl text-3xl font-bold mb-12 text-white text-center">
-        Thank you to our 2023-2024 Sponsors!
-      </h1>
-      <img
-        src={tshirt}
-        alt="placeholder"
-        className="mx-auto my-12 w-[70dvw] h-[80dvh]"
-      />
+    <div className="bg-black p-10">
+      <h1 className="text-white text-3xl font-bold text-center mb-2">Gold Sponsors</h1>
+      <hr />
+
+      <h2 className="text-white font-bold italic text-center text-3xl m-24">Coming Soon...</h2>
+
+      <h1 className="text-white text-3xl font-bold text-center mb-2">Silver Sponsors</h1>
+      <hr />
+
+      <h2 className="text-white font-bold italic text-center text-3xl m-24">Coming Soon...</h2>
+
+      <h1 className="text-white text-3xl font-bold text-center mb-2">Bronze Sponsors</h1>
+      <hr />
+
+      <h2 className="text-white font-bold italic text-center text-3xl m-24">Coming Soon...</h2>
+
+      <h1 className="text-white text-3xl font-bold text-center mb-2">Friends</h1>
+      <hr />
+      <div className="mt-2 mx-auto max-w-[72rem]">
+        <ul className="flex flex-col max-h-[30rem] flex-wrap gap-2">
+          {friends.map((friend) => (
+            <li className="text-white text-center" key={friend}>{friend}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
