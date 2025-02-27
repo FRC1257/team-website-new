b1961c6430d97b7bbee8276c5f05af5ca3bb03c0
derp000 <84947701+derp000@users.noreply.github.com>
2025-02-27 17:10:35 -0500

    Finish code for all sponsor tiers

--- SnailSite/src/pages/Sponsors.tsx
+++ SnailSite/src/pages/Sponsors.tsx
@@ -1,26 +1,36 @@
 import friends from "./sponsorFriendTier";
 import sponsorsBronze from "./sponsorsBronze";
+import sponsorsSilver from "./sponsorsSilver";
+import gold from "../assets/sponsors/gold/Handsome Energy Systems INC.png";
+import plat from "../assets/sponsors/platinum/Picatinny STEM.png";
 
 // refactor sponsorships into object with interface
 // using 18rem since max-w-[72rem] so 72/4 = 18
+// but factor in gap-5 aka 1.25rem so about 16 is good
 const BRONZE_SIZE: string = "w-[16rem] h-auto";
+const SILVER_SIZE: string = "w-[22rem] h-auto";
 
 const Sponsors = () => {
   return (
     <div className="bg-black p-10">
-      <h1 className="text-white text-3xl font-bold text-center mb-2">Gold Sponsors</h1>
-      <hr />
+      <h1 className="text-white text-3xl font-bold text-center mb-2">Platinum Sponsors</h1>
+      <hr className="mb-5" />
+      <img src={plat} alt="" className="w-[32rem] h-auto mx-auto mb-5" />
 
-      <h2 className="text-white font-bold italic text-center text-3xl m-24">Coming Soon...</h2>
+      <h1 className="text-white text-3xl font-bold text-center mb-2">Gold Sponsors</h1>
+      <hr className="mb-5" />
+      <img src={gold} alt="" className="w-[32rem] h-auto mx-auto bg-white mb-5" />
 
       <h1 className="text-white text-3xl font-bold text-center mb-2">Silver Sponsors</h1>
-      <hr />
-
-      <h2 className="text-white font-bold italic text-center text-3xl m-24">Coming Soon...</h2>
+      <hr className="mb-5" />
+      <div className="flex flex-row max-w-[72rem] flex-wrap justify-center mx-auto mb-5 gap-5">
+        <img src={sponsorsSilver[0]} alt="" className={SILVER_SIZE} />
+        <img src={sponsorsSilver[1]} alt="" className={SILVER_SIZE} />
+        <img src={sponsorsSilver[2]} alt="" className={SILVER_SIZE} />
+      </div>
 
       <h1 className="text-white text-3xl font-bold text-center mb-2">Bronze Sponsors</h1>
       <hr />
-
       <div className="flex flex-row max-w-[72rem] flex-wrap justify-center mx-auto mb-5 gap-5">
         <img src={sponsorsBronze[0]} alt="" className={BRONZE_SIZE} />
         <img src={sponsorsBronze[1]} alt="" className={BRONZE_SIZE} />
@@ -35,7 +45,7 @@ const Sponsors = () => {
       </div>
 
       <h1 className="text-white text-3xl font-bold text-center mb-2">Friends</h1>
-      <hr />
+      <hr className="mb-5" />
       <div className="mt-2 mx-auto max-w-[96rem]">
         <ul className="flex flex-col lg:max-h-[30rem] lg:flex-wrap gap-2">
           {friends.map((friend) => (
---
+++ SnailSite/src/pages/sponsorsSilver.ts
@@ -0,0 +1,7 @@
+import greyorange from "../assets/sponsors/silver/Grey Orange.png";
+import bagels from "../assets/sponsors/silver/Jersey Joes Bagels & Deli .png";
+import nokia from "../assets/sponsors/silver/Nokia Bell.png";
+
+const sponsorsSilver: string[] = [greyorange, bagels, nokia];
+
+export default sponsorsSilver;
