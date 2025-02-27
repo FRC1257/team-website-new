313419f913d6403c6d9a2af98922cf04a65942ed
derp000 <84947701+derp000@users.noreply.github.com>
2025-02-27 16:47:36 -0500

    Add code for bronze sponsors

--- SnailSite/src/pages/Sponsors.tsx
+++ SnailSite/src/pages/Sponsors.tsx
@@ -1,4 +1,9 @@
 import friends from "./sponsorFriendTier";
+import sponsorsBronze from "./sponsorsBronze";
+
+// refactor sponsorships into object with interface
+// using 18rem since max-w-[72rem] so 72/4 = 18
+const BRONZE_SIZE: string = "w-[16rem] h-auto";
 
 const Sponsors = () => {
   return (
@@ -16,11 +21,22 @@ const Sponsors = () => {
       <h1 className="text-white text-3xl font-bold text-center mb-2">Bronze Sponsors</h1>
       <hr />
 
-      <h2 className="text-white font-bold italic text-center text-3xl m-24">Coming Soon...</h2>
+      <div className="flex flex-row max-w-[72rem] flex-wrap justify-center mx-auto mb-5 gap-5">
+        <img src={sponsorsBronze[0]} alt="" className={BRONZE_SIZE} />
+        <img src={sponsorsBronze[1]} alt="" className={BRONZE_SIZE} />
+        <img src={sponsorsBronze[2]} alt="" className={BRONZE_SIZE} />
+        <img src={sponsorsBronze[3]} alt="" className={BRONZE_SIZE} />
+        <img src={sponsorsBronze[4]} alt="" className={BRONZE_SIZE} />
+        <img src={sponsorsBronze[5]} alt="" className={BRONZE_SIZE + " bg-white"} />
+        <img src={sponsorsBronze[6]} alt="" className={BRONZE_SIZE + " bg-white"} />
+        <img src={sponsorsBronze[7]} alt="" className={BRONZE_SIZE + " bg-white"} />
+        <img src={sponsorsBronze[8]} alt="" className={BRONZE_SIZE + " bg-white"} />
+        <img src={sponsorsBronze[9]} alt="" className={BRONZE_SIZE + " bg-white"} />
+      </div>
 
       <h1 className="text-white text-3xl font-bold text-center mb-2">Friends</h1>
       <hr />
-      <div className="mt-2 mx-auto max-w-[72rem]">
+      <div className="mt-2 mx-auto max-w-[96rem]">
         <ul className="flex flex-col lg:max-h-[30rem] lg:flex-wrap gap-2">
           {friends.map((friend) => (
             <li className="text-white text-center" key={friend}>{friend}</li>
